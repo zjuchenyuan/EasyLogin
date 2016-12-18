d874f4b276198c58aad4ecfd5dd1321e175dba30
zjuchenyuan <chenyuan@zju.edu.cn>
2016-12-18 11:49:47 +0800

    亿方云API

---
+++ examples/fangcloud2/README.md
@@ -0,0 +1,31 @@
+## 亿方云API
+
+使用EasyLogin完成“亿方云”登录、上传、分享与下载直连获取一条龙
+
+### 使用方法
+
+这是EasyLogin的使用示例，请先访问https://github.com/zjuchenyuan/EasyLogin
+
+下载好EasyLogin.py，并完成依赖安装
+
+    python3 run.py 待上传的文件名 亿方云用户名 亿方云密码
+
+![screenshot](screenshot.png)
+
+### 说明
+
+请先注册亿方云 https://www.fangcloud.com/auth/register ，体验版20G存储
+
+本程序会自动保存、载入登录状态到fangcloud.status（使用EasyLogin提供的**save**与**load**函数）
+
+所以在cookie没有失效时，不会发起登录请求。如图所示，此时可以不提供用户名密码参数
+
+#### 函数说明
+
+```
+login(xh,password) #用户名密码登录亿方云
+islogin() #是否已经登录,如果已经登录返回token，否则False
+upload(token,filename,data) #上传文件，返回fileid
+share(token,fileid) #分享文件，返回分享地址file_unique_name
+download(file_uniqe_name) #从文件分享地址得到直接下载链接URL，本函数不要求登录，也不会使用登录状态
+```
---
+++ examples/fangcloud2/run.py
@@ -0,0 +1,113 @@
+from EasyLogin import EasyLogin
+import re
+from pprint import pprint
+
+a=EasyLogin.load("fangcloud.status")
+
+
+def login(username,password):
+    """
+    使用账号(邮箱)和密码，选择“记住我”登录
+    :param username:
+    :param password:
+    :return:
+    """
+    global a
+    a.get("https://www.fangcloud.com/auth/login")
+    token=a.b.find("input",{"name":"requesttoken"})["value"]
+    x=a.post("https://www.fangcloud.com/auth/login",
+             """{"identifier":"%s","password":"%s","remember_login":"1","scene":"login"}"""%(username,password),
+             headers={"requesttoken":token,"X-Requested-With":"XMLHttpRequest"})
+    result=x.json()
+    if result["success"]!=True:
+        return False
+    else:
+        return True
+
+def islogin():
+    """
+    是否已经登录,如果已经登录返回token，否则False
+    """
+    global a
+    a.get("https://www.fangcloud.com/apps/files/home")
+    t=a.b.find("input",{"id":"oc_requesttoken"})
+    if t is None:
+        return False
+    else:
+        return t["value"]
+
+
+def upload(token,filename,data):
+    """
+    上传文件，与浙大云盘的差异在于上传链接需要额外的请求
+    token:islogin()返回的token
+    filename: 存储的文件名
+    data:文件二进制数据
+
+    返回服务器端的文件id
+    """
+    global a
+    x=a.post("https://www.fangcloud.com/apps/files/presign_upload",
+             """{"folder_id":0,"file_size":%d}"""%len(data),
+             headers={"requesttoken": token, "X-Requested-With": "XMLHttpRequest"})
+    result=x.json()
+    if result["success"]!=True:
+        return False
+    upload_url=result["upload_url"]
+    x=a.post(upload_url,
+             data,
+             headers={"requesttoken": token,"X-File-Name":filename})
+    result=x.json()
+    if result["success"]!=True:
+        return False
+    return result["new_file"]["typed_id"]
+
+def share(token,fileid):
+    """
+    分享一个文件，fileid来自upload，返回文件分享链接file_unique_name
+    可以反复执行，返回相同的分享链接
+    """
+    global a
+    x=a.post("https://www.fangcloud.com/apps/files/share",
+             """{"access": "public", "disable_download": "0", "due_time": "never_expire", "password_protected": false,"item_typed_id": "%s"}"""%fileid,
+             headers={"requesttoken":token,"X-Requested-With": "XMLHttpRequest"})
+    result=x.json()
+    if result.get("success")!=True:
+        return False
+    else:
+        return result["share_link"]["unique_name"]
+
+finder=re.compile(r'''file_(\d+)''')
+
+def download(file_uniqe_name):
+    """
+    获得一个分享链接的直接链接
+    本函数可能被频繁调用，为优化性能不使用BeautifulSoup
+    :param file_uniqe_name: 分享链接，来自share
+    :return: 可以直接下载的url，一段时间后失效
+    """
+    a=EasyLogin()
+    page=a.get("https://www.fangcloud.com/share/"+file_uniqe_name,result=False)
+    fileid = finder.search(page).group(1)
+    x=a.get("https://www.fangcloud.com/apps/files/download?file_id={}&scenario=share".format(fileid),o=True)
+    return x.headers["Location"]
+
+
+if __name__=="__main__":
+    import sys
+    token = islogin()
+    if not token:
+        print("Login!")
+        login(sys.argv[2],sys.argv[3])
+        a.save("fangcloud.status")
+        token=islogin()
+    fileid=upload(token,sys.argv[1],open(sys.argv[1],"rb").read())
+    file_uniqe_name=share(token,fileid)
+    print("fileid:")
+    print(fileid)
+    print()
+    print("Share Link:")
+    print("https://www.fangcloud.com/share/"+file_uniqe_name)
+    print()
+    print("Download Link (expire soon):")
+    print(download(file_uniqe_name))
