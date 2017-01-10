bfc734c8f679abaeea0c0206e75f53ea674646c8
zjuchenyuan <chenyuan@zju.edu.cn>
2017-01-10 18:48:33 +0800

    fix cookistring headers None

--- EasyLogin.py
+++ EasyLogin.py
@@ -117,6 +117,8 @@ class EasyLogin:
                 headers = {"Accept-Encoding": "gzip, deflate, sdch", "Accept-Language": "zh-CN,zh;q=0.8", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "Upgrade-Insecure-Requests": "1", "DNT": "1"}
             headers["Referer"] = "/".join(url.split("/")[:3])
         if cookiestring is not None:
+            if headers is None:
+                headers = {}
             headers["Cookie"] = cookiestring
         x = self.s.get(url, headers=headers, allow_redirects=False, proxies=self.proxies)
         if result:
