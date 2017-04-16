d49f63237286d4505e575dd8e81949d35f33d6b3
zjuchenyuan <chenyuan@zju.edu.cn>
2017-04-16 13:53:07 +0800

    Update cyserver

--- examples/fangcloud/cyserver.py
+++ examples/fangcloud/cyserver.py
@@ -3,21 +3,22 @@ import http.server as BaseHTTPServer
 class CYServer():
     class APIHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         def _hello(self):
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write("hello world!".encode('ascii'))
+            self.server_write_html("hello world!")
 
         def _302(self,url):
             self.wfile.write(("HTTP/1.1 302 Found\r\nLocation: "+url+"\r\n\r\n").encode('ascii'))
 
-        def _404(self):
-            self.wfile.write("Not Found")
-
         def _fail(self,e):
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(("Error:"+str(e)).encode('ascii'))
+            self.server_write_html("Error: "+str(e),content_type="text/plain")
 
+        def server_write_html(self,html,content_type="text/html"):
+            data=bytes(str(html),encoding="utf-8")
+            self.server_write_bytes(data,content_type=content_type)
+
+        def server_write_bytes(self,data,content_type="image/gif"):
+            content=bytes("HTTP/1.0 200 OK\r\nContent-Type: {type}; charset=utf-8\r\nContent-Length: {size}\r\nServer: CYServer\r\n\r\n".format(type=content_type,size=len(data)),encoding="utf-8")+data
+            self.wfile.write(content)
+    
     def __init__(self, port, do_GET, do_POST=None, do_HEAD=None, bind_address="0.0.0.0"):
         self.port=port
         self.do_GET=do_GET
