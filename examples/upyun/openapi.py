abfefa21992d8c521a7632a6f364335a01cad0a7
zjuchenyuan <chenyuan@zju.edu.cn>
2020-01-21 16:36:25 +0800

    [fix] fix ACCESS_TOKEN import; add purge function

--- examples/upyun/openapi.py
+++ examples/upyun/openapi.py
@@ -1,6 +1,6 @@
 from upyun import login, islogin, a
 import random, string
-from config import USERNAME, PASSWORD, ACCESS_TOKEN
+from config import USERNAME, PASSWORD
 
 def createtoken(name):
     x=a.post("https://api.upyun.com/oauth/tokens", {
@@ -13,8 +13,14 @@ def createtoken(name):
     return x.json()['access_token']
 
 def query(url):
+    from config import ACCESS_TOKEN
     x=a.get("https://api.upyun.com/"+url, headers={"Authorization":"Bearer "+ACCESS_TOKEN}, o=True, result=False)
     print(x.request.url)
+    return x.json()
+
+def post(url, data):
+    from config import ACCESS_TOKEN
+    x=a.post("https://api.upyun.com/"+url, headers={"Authorization":"Bearer "+ACCESS_TOKEN}, result=False, data=data)
     return x.json()
 
 def getalldomain():
@@ -23,6 +29,9 @@ def getalldomain():
     for b in buckets:
         res.extend([i["domain"] for i in b["domains"] if i["status"]=="NORMAL" and (not i["domain"].endswith(".upcdn.net")) and (not i["domain"].endswith(".upaiyun.com")) ])
     return res
+
+def purge(urls):
+    return post("purge", {"urls":"\n".join(urls)})
 
 if __name__ == "__main__":
     from pprint import pprint
